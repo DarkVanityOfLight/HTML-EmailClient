126582cd112c72891706c8707dfb232901042afd
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-07-31 15:25:33 +0200

    feat: Implement a function to send emails via a smtp server

---
+++ send_email.py
@@ -0,0 +1,18 @@
+import smtplib, ssl
+from email.message import EmailMessage
+
+port = 465
+
+
+
+def send(sender_email, password, receiver_email, server, message):
+    context = ssl.create_default_context()
+
+    if isinstance(message, EmailMessage):
+        message = message.as_string()
+
+    with smtplib.SMTP_SSL(server, port, context=contexxt) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+
