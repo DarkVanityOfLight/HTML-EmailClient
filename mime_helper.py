bdeff236c44a9aeb2a500f1686c04b6178cf5783
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-08-04 12:11:55 +0200

    chore: Split up the mime multipart

--- mime_helper.py
+++ mime_helper.py
@@ -7,17 +7,19 @@ def create_html_part(html):
 def create_plain_part(text):
     return MIMEText(text, "plain")
 
-def create_multipart(*args):
+def create_mixed_multipart(*args):
     return MIMEMultipart(_subparts=args)
 
+def create_alternative_multipart(*args)
+    return MIMEMultipart(_subparts=args, _subtype="alternative")
 
 if __name__ == "__main__":
 
     from email.message import EmailMessage
 
-    part1 = mime_helper.create_html_part("Foo")
-    part2 = mime_helper.create_plain_part("Bar")
-    multipart = mime_helper.create_multipart(part1, part2)
+    part1 = create_html_part("Foo")
+    part2 = create_plain_part("Bar")
+    multipart = create_mixed_multipart(part1, part2)
     msg = EmailMessage()
     msg.set_content(multipart)
 
