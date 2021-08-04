8773dc7ed019eda609803ee14bb96013664e10c6
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-08-04 12:46:36 +0200

    fix: Remove an x from contexxt

--- send_email.py
+++ send_email.py
@@ -11,7 +11,7 @@ def send(sender_email, password, receiver_email, server, message):
     if isinstance(message, EmailMessage):
         message = message.as_string()
 
-    with smtplib.SMTP_SSL(server, port, context=contexxt) as server:
+    with smtplib.SMTP_SSL(server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
