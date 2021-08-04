4ca8ef75611487dd5518e40eb90c06ecb19d700d
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2021-08-04 14:27:46 +0200

    chore: Create methods to change the view to login or editor

--- main_view.py
+++ main_view.py
@@ -21,21 +21,15 @@ class MainView(QQuickView):
 
         self.login_view_file = Path(__file__).parent / "login.qml"
         self.editor_view_file = Path(__file__).parent / "editor.qml"
-        self.setSource(QUrl.fromLocalFile(os.fspath(self.login_view_file.resolve())))
 
-
-        self.column = self.rootObject()
-
-        self.findLoginFields(self.column)
-        self.login_button.clicked.connect(self.setCreds)
+        self.makeLogin()
 
     def setCreds(self):
         self.email = self.email_field.property("text")
         self.password = self.password_field.property("text")
         self.smtp = self.smtp_field.property("text")
 
-        self.setSource(QUrl.fromLocalFile(os.fspath(self.editor_view_file.resolve())))
-
+        self.makeEditor()
 
     def findLoginFields(self, parent):
         self.email_field = parent.findChild(QObject, "emailField")
@@ -43,5 +37,13 @@ class MainView(QQuickView):
         self.smtp_field = parent.findChild(QObject, "smtpField")
         self.login_button = parent.findChild(QObject, "loginButton")
 
+    def makeEditor(self):
+        self.setSource(QUrl.fromLocalFile(os.fspath(self.editor_view_file.resolve())))
 
+    def makeLogin(self):
+        self.setSource(QUrl.fromLocalFile(os.fspath(self.login_view_file.resolve())))
 
+        self.column = self.rootObject()
+
+        self.findLoginFields(self.column)
+        self.login_button.clicked.connect(self.setCreds)
